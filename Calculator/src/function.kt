e4cb3cfd3a36b937f8b45d02772d2f09e8bba71d
Alan Bosco <justafreak15@gmail.com>
2017-09-25 00:25:36 +0530

    Calculator

---
+++ Calculator/src/function.kt
@@ -0,0 +1,16 @@
+package Caluculator.function
+
+fun addition(a:Int, b:Int) :Int{
+    return a+b
+}
+fun multiplication(a:Int, b:Int):Int{
+    return a*b
+}
+
+fun substraction(a:Int, b:Int):Int{
+    return a-b
+}
+
+fun division(a: Float, b: Float):Float{
+    return a/b
+}
